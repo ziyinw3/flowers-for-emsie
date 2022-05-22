05397f116ce223c90d1c34c22c15127eb46c0442
ziyinw3 <ziyinw3@illinois.edu>
2022-05-21 20:20:55 -0500

    implemented idle response for space key press

---
+++ hexagonal flower maze/test files/continous reading of key input.py
@@ -0,0 +1,35 @@
+import pygame
+import sys
+
+pygame.init()
+
+size = (640, 480)
+white = (225, 225, 225)
+clock = pygame.time.Clock()
+FPS = 60
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Continous Reading test")
+
+gameQuit = False
+
+standcount = 0
+
+while not gameQuit:
+    # "FPS, lower faster"
+    clock.tick(2)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameQuit = True
+            pygame.quit()
+            sys.exit()
+
+    key_state = pygame.key.get_pressed()
+    if key_state[pygame.K_SPACE] == False:
+        standcount += 1
+        if standcount > 6:
+            standcount = 1
+    print(standcount)
+    # standcount increments per tick, aka. response feedback time
+    # if key is not pressed, idle will loop through as usual. if key is pressed, idle will not increment
+    
