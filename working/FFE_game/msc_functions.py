8178a1c0eb730f587589027f8157bdeaf5e36090
ziyinw3 <ziyinw3@illinois.edu>
2022-06-12 18:05:28 -0500

    y

--- working/FFE_game/msc_functions.py
+++ working/FFE_game/msc_functions.py
@@ -3,6 +3,9 @@ pygame.init()
 
 from game_settings import *
 
+
+# attributes update
+
 def ks_listener():
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
@@ -37,21 +40,26 @@ def loc_listener():
 def walk_counter():
     # find first item in states that is true
     for item in states:
-        if item == False:
-            continue
-        face_dir = states.index(item)
+        if item == True:
+            face_dir = states.index(item)
+            idle = False
+    if idle == False:    
         if walkcount < 16:
             walkcount += 1
         elif walkcount == 17:
             walkcount = 1
-    if standcount < 12:
-        standcount += 1
-    elif standcount == 13:
-        standcount = 1
-    walkcount = 0
-    face_dir = 3
+    if idle == True:
+        if standcount < 12:
+            standcount += 1
+        elif standcount == 13:
+            standcount = 1
+        walkcount = 0
+        face_dir = 3
+
+# update screen
 
 # def blit_mc():
+
 
 
 
