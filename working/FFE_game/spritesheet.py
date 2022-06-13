1a26fe4061415bf54137daebd15dedd65dec46f4
ziyinw3 <ziyinw3@illinois.edu>
2022-06-13 14:26:15 -0500

    start load_strip function

--- working/FFE_game/spritesheet.py
+++ working/FFE_game/spritesheet.py
@@ -2,20 +2,14 @@
 
 import pygame
 
-
 class SpriteSheet:
 
     def __init__(self, filename):
         """Load the sheet."""
-
-    def image_at(self, rectangle):
-        """Load a specific image from a specific rectangle."""
-        # Loads image from x, y, x+offset, y+offset.
-        rect = pygame.Rect(rectangle)
-        image = pygame.Surface(rect.size, pygame.SRCALPHA).convert_alpha()
-        image.blit(self.sheet, (0, 0), rect)
-        return image
-
-    def images_at(self, rects):
-        """Load a whole bunch of images and return them as a list."""
-        return [self.image_at(rect) for rect in rects]
+        self.filename = filename
+        self.spritesheet = pygame.image.load(filename)
+    def load_strip(self, rects, lis):
+        for rect in rects:
+            sprite = pygame.Surface((41, 41), pygame.SRCALPHA).convert_alpha()
+            sprite.blit(self.spritesheet,(0, 0), rect)
+            lis.append(sprite)
