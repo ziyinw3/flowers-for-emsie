8f1ce584b4275e7db94c9371fa68860a17ea2d4b
ziyinw3 <ziyinw3@illinois.edu>
2022-06-16 20:30:20 -0500

    do work on main menu

--- working/FFE_game/main_menu.py
+++ working/FFE_game/main_menu.py
@@ -3,11 +3,19 @@ from settings import Settings
 
 import pygame
 
-start_btn = pygame.image.load(os.path.join("game_assets", "button_play.png")).convert_alpha()
-logo = pygame.image.load(os.path.join("game_assets", "logo.png")).convert_alpha()
+start_btn = pygame.image.load()
+logo = pygame.image.load()
 
 class MainMenu:
     def __init__(self, screen):
     # self.bg = a set of images for animated menu
-    self.screen = screen
+    # self.screen = screen
     self.btn = (self.width/2 - start_btn.get_width()/2, 350, start_btn.get_width(), start_btn.get_height())
+
+    def run(self):
+        run = True
+        while run:
+             for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+
