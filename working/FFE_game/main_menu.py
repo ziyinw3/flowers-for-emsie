c306ed94c788a04ef51e94d91a490e021c6db878
ziyinw3 <ziyinw3@illinois.edu>
2022-06-17 10:21:40 -0500

    main menu blitted plus custom cursor

--- working/FFE_game/main_menu.py
+++ working/FFE_game/main_menu.py
@@ -9,14 +9,21 @@ class MainMenu:
     def __init__(self, screen):
         self.screen = screen
         self.clock = pygame.time.Clock()
+        self.mm_count = 0 
 
     def run(self):
         run = True
         while run:
-            self.clock.tick(2)
+            self.clock.tick(60)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
+            if self.mm_count < 179:
+                self.mm_count += 1
+            else:
+                self.mm_count = 0
+            self.screen.blit(mm[self.mm_count // 30], (0, 0))
             cursor_rect.center = pygame.mouse.get_pos()  # update position 
             self.screen.blit(cursor, cursor_rect)
+            pygame.display.flip()
 
