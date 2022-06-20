5d63932faf10589d8645a6fc0c37060bf982cf55
ziyinw3 <ziyinw3@illinois.edu>
2022-06-19 20:28:46 -0500

    add assets and notes on options page

--- working/FFE_game/options.py
+++ working/FFE_game/options.py
@@ -4,3 +4,10 @@ class OptPage:
 
     def run(self):
         print('running options page')
+        # start while loop, check for q
+        # blit option screen loop based on step count
+        # mouse visible false, blit custom cursor
+        # write button hovers, sounds, and store button clicked state and blit clicked image
+
+
+        # write back button that can thread back to previous screen??
