116085e61166f85b22604509d374ada6b7ab4870
ziyinw3 <ziyinw3@illinois.edu>
2022-07-02 15:21:56 -0500

    refining video seeker

--- working/FFE_game/intro.py
+++ working/FFE_game/intro.py
@@ -7,7 +7,7 @@ from settings import settings
 from pyvidplayer import Video
 
 screen = pygame.display.set_mode((settings.screen_width, settings.screen_height))
-main_cutscene = Video("FFE_game\\videos\\timer_vid.mp4")
+main_cutscene = Video("FFE_game\\videos\\sample_vid.mp4")
 main_cutscene.set_size((640, 480))
 
 class Intro:
@@ -15,8 +15,8 @@ class Intro:
         self.screen = screen
         self.clock = pygame.time.Clock()
         self.click_count = 0
-        self.elapsed = 0
-        self.seek_time = 0
+        self.seek_time = 5
+        self.pos = 60
     
     def run(self):
         run = True
@@ -24,7 +24,9 @@ class Intro:
         # listen to settings here
 
         while run:
+            #fps for detecting input
             self.clock.tick(60)
+                # increment timer by 1 sec
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     #release video resources when done
@@ -34,17 +36,15 @@ class Intro:
                     if event.key == pygame.K_q:
                         sys.exit()
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    if self.click_count < 12:
-                        self.elapsed = pygame.time.get_ticks() / 1000
-                        if self.elapsed > 5:
-                            self.seek_time = 5 % self.elapsed
-                        else:
-                            self.seek_time = (5 - self.elapsed)
-                        print(self.elapsed, self.seek_time, self.elapsed + self.seek_time)
+                    if self.pos - self.seek_time > 0:
+                        self.seek_time = (5 - self.pos % 5)
+                        self.pos -= self.seek_time                        
                         main_cutscene.seek(self.seek_time)
+                    print(self.pos, self.seek_time)
                     
             
             main_cutscene.draw(screen, (0, 0), force_draw=False)
+            self.pos -= 1/60
             
             pygame.display.update()
 
