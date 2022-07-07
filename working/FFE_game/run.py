4a6e399d5a7bf82cdc9e831d14227b367d994735
ziyinw3 <ziyinw3@illinois.edu>
2022-07-06 20:18:56 -0500

    add flags for testing

--- working/FFE_game/run.py
+++ working/FFE_game/run.py
@@ -2,7 +2,7 @@ import pygame
 
 if __name__ == "__main__":
     pygame.init()
-    screen = pygame.display.set_mode((640, 480))
+    screen = pygame.display.set_mode((640, 480), pygame.FULLSCREEN|pygame.SCALED)
     from main_menu import MainMenu
     mainMenu = MainMenu(screen)
     mainMenu.run()
